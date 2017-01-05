cbbac718f23b6e93bd1b6a4c29c3faa65d0e048a
Evgeny Ivanov <busforward@gmail.com>
2017-01-05 21:58:45 +0500

    Add template

---
+++ config.rb
@@ -0,0 +1,10 @@
+http_path = ".."
+css_dir = "css" # by Compass.app 
+sass_dir = "scss" # by Compass.app 
+images_dir = "img" # by Compass.app 
+output_style = :compressed # by Compass.app 
+relative_assets = false # by Compass.app 
+line_comments = false # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = false # by Compass.app
+cache = false
---
+++ js/function.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+	// First screen full height
+	function setHeiHeight() {
+	    $('.full__height').css({
+	        minHeight: $(window).height() + 'px'
+	    });
+	}
+	setHeiHeight(); // устанавливаем высоту окна при первой загрузке страницы
+	$(window).resize( setHeiHeight ); // обновляем при изменении размеров окна
+
+
+	// Reset link whte attribute href="#"
+	$('[href*="#"]').click(function(event) {
+		event.preventDefault();
+	});
+
+	// Scroll to ID // Плавный скролл к элементу при нажатии на ссылку. В ссылке указываем ID элемента
+	// $('#main__menu a[href^="#"]').click( function(){ 
+	// 	var scroll_el = $(this).attr('href'); 
+	// 	if ($(scroll_el).length != 0) {
+	// 	$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+	// 	}
+	// 	return false;
+	// });
+
+	// Stiky menu // Липкое меню. При прокрутке к элементу #header добавляется класс .stiky который и стилизуем
+    // $(document).ready(function(){
+    //     var HeaderTop = $('#header').offset().top;
+        
+    //     $(window).scroll(function(){
+    //             if( $(window).scrollTop() > HeaderTop ) {
+    //                     $('#header').addClass('stiky');
+    //             } else {
+    //                     $('#header').removeClass('stiky');
+    //             }
+    //     });
+    // });
+
+    $('.icon_drop_menu').on('click', function() {
+    	event.preventDefault();
+    	$('.header_drop').addClass('open');
+    });
+
+   	$('.drop_menu .icon_close').on('click', function() {
+   		event.preventDefault();
+    	$('.header_drop').removeClass('open');
+   	});
+
+});
