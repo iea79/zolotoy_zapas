c5a0adb52e1b43ad6c99ca2f9fc4ca0a02ebcc15
Evgeny Ivanov <busforward@gmail.com>
2017-01-10 11:41:51 +0500

    Index page

--- js/function.js
+++ js/function.js
@@ -47,4 +47,81 @@ $(document).ready(function() {
     	$('.header_drop').removeClass('open');
    	});
 
+	$('.slider_image').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.slider_text'
+	});
+	$('.slider_text').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		asNavFor: '.slider_image',
+		dots: true,
+		arrows: false,
+		fade: true,
+		centerMode: false,
+		focusOnSelect: true
+	});
+
+	$('.coin_slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		arrows: true,
+		dots: false,
+		prevArrow: '<div class="arrow_prev"></div>',
+		nextArrow: '<div class="arrow_next"></div>',
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+				}
+			},
+			{
+				breakpoint: 820,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				}
+			},
+			{
+				breakpoint: 599,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+			},
+		]
+	})
+
+	var boxHei = $('.box_preview').height();
+		boxWidth = $('.box_preview').width();
+
+	$(window).resize(function() {
+		var boxHei = $('.box_preview').height();
+			boxWidth = $('.box_preview').width();
+		$('.grid__item').removeClass('open').removeAttr('style');
+		$('.box_preview').removeAttr('style');
+	});
+
+	$('.box_preview').on('click', '.more_detailed', function(event) {
+		event.preventDefault();
+
+		$('.box_preview').css({
+			minHeight: boxHei,
+		});
+		$(this).closest('.grid__item').addClass('open').css({
+			width: boxWidth,
+			minHeight: boxHei,
+		});
+	});
+
+	$('.box_preview').on('click', '.close_detailed', function(event) {
+		event.preventDefault();
+		$(this).closest('.grid__item').removeClass('open').removeAttr('style');
+	});
+
 });
